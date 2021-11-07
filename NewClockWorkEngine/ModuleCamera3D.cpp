3dfcdada5d616f829ab7d8a9fb8619f2c6c4a571
Dani <45202338+xsiro@users.noreply.github.com>
2021-11-07 18:27:18 +0100

    F BUTTON FUNCTIONALITY

--- NewClockWorkEngine/ModuleCamera3D.cpp
+++ NewClockWorkEngine/ModuleCamera3D.cpp
@@ -46,8 +46,10 @@ update_status ModuleCamera3D::Update(float dt)
 		speed = 8.0f * dt;
 
 
-	if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
-	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos.y -= speed;
+	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos.y += speed;
+	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos.y -= speed;
+
+	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_DOWN) LookAt({ 0, 0, 0 });
 
 	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
 	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
